556e59e999c3c09756ed3a4aa5fa0f8a51973b97
algolstep <jiemianwang@gmail.com>
2015-11-04 13:29:07 +0800

    Initial Commit

---
+++ TestStructureDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestStructureDemo
+//
+//  Created by 思来氏 on 15/11/4.
+//  Copyright (c) 2015年 ThornDemo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TestStructureDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestStructureDemo
+//
+//  Created by 思来氏 on 15/11/4.
+//  Copyright (c) 2015年 ThornDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
